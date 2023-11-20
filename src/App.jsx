6f8e1b6880101a25e7b6b306352bf0784fd216a7
alishowsilabas <alissonsilabas@gmail.com>
2023-11-20 08:01:53 -0300

    reforçando hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
       <Nome nomeAluno="Adair" />
-      <Nome nomeAluno="Joarez" />
     </>
   );
 }
--- src/assets/components/Nome.jsx
+++ src/assets/components/Nome.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Nome({ nomeAluno }) {
+  const [aluno, setNomeAluno] = useState("Nome Genérico");
+  function handleChangeName(nome) {
+    setNomeAluno(nome);
+  }
+
   return (
     <div>
       <h1>Aluno: {nomeAluno}</h1>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName("Baltazar da Silva")}>Mudar Nome</button>
     </div>
   );
 }
